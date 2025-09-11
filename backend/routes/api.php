2b9771046c80aaf1c323581875960c153ec577e0
Thiago Rossi <thmrdev@gmail.com>
2025-09-10 21:47:07 -0300

    feat(sale): add SaleController and api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SellerController; 
+use App\Http\Controllers\SaleController; 
 
 Route::apiResource('sellers', SellerController::class)
     ->only(['index', 'store']);
+
+Route::apiResource('sales', SaleController::class)
+    ->only(['index', 'store']);
+
+Route::get('sellers/{seller}/sales', [SaleController::class, 'salesBySeller']);
